5ab205febfa5de4239ac9f0b55b829b5adcea272
jmhmjayamaha <jmhmjayamaha@gmail.com>
2018-03-11 09:32:51 +0530

    web service configuration bean is added

---
+++ src/main/java/lk/harshana/config/WebServiceConfig.java
@@ -0,0 +1,42 @@
+package lk.harshana.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@Configuration
+@EnableWs
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+	@Bean
+	public XsdSchema personSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("xsds/person.xsd"));
+	}
+
+	@Bean
+	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(applicationContext);
+		servlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean(servlet, "/soapws/*");
+	}
+
+	@Bean(name = "persons")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema personsSchema) {
+		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+		wsdl11Definition.setPortTypeName("personPort");
+		wsdl11Definition.setLocationUri("/soapws");
+		wsdl11Definition.setTargetNamespace("http://www.example.com/person-ws");
+		wsdl11Definition.setSchema(personsSchema);
+		return wsdl11Definition;
+	}
+
+}
